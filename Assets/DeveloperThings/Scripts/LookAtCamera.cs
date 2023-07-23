fa53c3c6ddbe65d46ce2985e14f0b6fb9f89387c
Samet Şekerci <95055963+WithRomance@users.noreply.github.com>
2023-07-23 05:07:10 +0300

    Gold Pop-Up

--- Assets/DeveloperThings/Scripts/LookAtCamera.cs
+++ Assets/DeveloperThings/Scripts/LookAtCamera.cs
@@ -6,7 +6,7 @@ public class LookAtCamera : MonoBehaviour
 {
     void LateUpdate()
     {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * -Vector3.up);
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
 
     }
 }
