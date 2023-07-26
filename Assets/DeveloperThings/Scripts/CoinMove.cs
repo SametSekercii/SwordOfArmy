dfc42774155b2542cafcc78a24fcf841f80db47f
Samet Şekerci <95055963+WithRomance@users.noreply.github.com>
2023-07-26 15:35:39 +0300

    Revert "TrophyPopUp"
    This reverts commit d67ecfc2b85a2a17460f0670c0eddab03a7e820d.

--- Assets/DeveloperThings/Scripts/CoinMove.cs
+++ Assets/DeveloperThings/Scripts/CoinMove.cs
@@ -10,10 +10,12 @@ public class CoinMove : MonoBehaviour
     [SerializeField] private TMP_Text coinText;
     private Vector3 startedTransform;
 
+    private Camera mainCamera;
 
     private void OnEnable()
     {
         startedTransform = new Vector3(0.5f, 0.5f, 0.5f);
+        mainCamera = Camera.main;
         coinText = transform.GetChild(0).GetComponent<TMP_Text>();
         iconTransform = GameManager.Instance.GetMoneyIconTransform();
         StartMove();
@@ -32,4 +34,12 @@ public class CoinMove : MonoBehaviour
     }
 
     public void SetCoinText(float value) => coinText.text = value.ToString();
+    // private Vector3 GetTargetPosition()
+    // {
+    //     Vector3 uiPos = iconTransform.position;
+    //     uiPos.z = (transform.position - mainCamera.transform.position).z;
+    //     Vector3 result = mainCamera.ScreenToWorldPoint(uiPos);
+    //     return result;
+
+    // }
 }
